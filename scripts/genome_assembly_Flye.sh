91c71fec371ac6b05ccd5fc46de5e0bf679d3db9
RaziyehMohseni <a20razmo@student.his.se>
2024-05-06 20:10:29 +0200

    updated the scripts

--- scripts/genome_assembly_Flye.sh
+++ scripts/genome_assembly_Flye.sh
@@ -4,7 +4,7 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 15:00:00
+#SBATCH -t 5:00:00
 #SBATCH -J flye_genome_assembly
 #SBATCH --mail-type=ALL 
 #SBATCH --mail-user ramo1628@student.uu.se
@@ -15,4 +15,4 @@ module load bioinfo-tools
 module load Flye
 
 # Your commands 
-flye --pacbio-raw /home/ramo1628/genome_project/data/raw_data/pacbio_data/SRR6037732_scaffold_06.fq.gz --out-dir /home/ramo1628/genome_project/outputs/genome_assembly/
+flye --pacbio-raw /home/ramo1628/genome_project/data/raw_data/pacbio_data/SRR6037732_scaffold_06.fq.gz --out-dir /home/ramo1628/genome_project/outputs/genome_assembly/ --threads 4
